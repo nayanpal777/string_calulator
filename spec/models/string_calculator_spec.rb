6fd0116408c20e37ccb3ee007973483cdd61bd78
nayan.pal <v-nayan.pal@shutterfly.com>
2025-06-13 10:21:55 +0530

    v2 - written all the failed test cases for add method

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe StringCalculator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.add' do
+    it 'returns 0 for an empty string' do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+
+    it 'returns 0 for an nil value' do
+      expect(StringCalculator.add(nil)).to eq(0)
+    end
+
+    it 'returns the number itself for single number' do
+      expect(StringCalculator.add("5")).to eq(5)
+    end
+
+    it 'returns the sum for two numbers' do
+      expect(StringCalculator.add("2,3")).to eq(5)
+    end
+
+    it 'returns the sum for multiple numbers' do
+      expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
+  end
 end
