4e7e02894b278ddaf9931fd5bf394d278d83c8fc
nayan.pal <v-nayan.pal@shutterfly.com>
2025-06-13 15:56:49 +0530

    v7 - added code to handle delimiter

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -3,7 +3,15 @@ class StringCalculator < ApplicationRecord
   def self.add(input)
     return 0 if input.nil? || input&.strip&.empty?
 
-    numbers = input.gsub("\n", ",").split(",").map(&:strip)
+    delimiter = /,|\n/
+
+    if input.start_with?("//")
+      start_str, input = input.split("\n", 2)
+      custom_delimiter = start_str[2..]  # everything after //
+
+      delimiter = Regexp.union(custom_delimiter, ",", "\n")
+    end
+    numbers = input.split(delimiter).map(&:strip)
 
     numbers.map(&:to_i).sum
   end
