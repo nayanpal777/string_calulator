39b438f02c881b1a75c551400013bbcf4ed2cd18
nayan.pal <v-nayan.pal@shutterfly.com>
2025-06-13 10:38:41 +0530

    v5 - handle new lines between numbers and commas

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -3,7 +3,7 @@ class StringCalculator < ApplicationRecord
   def self.add(input)
     return 0 if input.nil? || input&.strip&.empty?
 
-    numbers = input.split(",").map(&:strip)
+    numbers = input.gsub("\n", ",").split(",").map(&:strip)
 
     numbers.map(&:to_i).sum
   end
