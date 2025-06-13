6c7919f58db8829e0118a370f06872990cec2595
nayan.pal <v-nayan.pal@shutterfly.com>
2025-06-13 10:23:15 +0530

    v3 - added add method for failed test cases

--- app/models/string_calculator.rb
+++ app/models/string_calculator.rb
@@ -1,2 +1,10 @@
 class StringCalculator < ApplicationRecord
+
+  def self.add(input)
+    return 0 if input.nil? || input&.strip&.empty?
+
+    numbers = input.split(",").map(&:strip)
+
+    numbers.map(&:to_i).sum
+  end
 end
